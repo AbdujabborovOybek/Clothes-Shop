38ffd1a1fd7a982afa48dae595166bc9561dc00d
OybekAbduljabbor <abdujabborovoybek@gmail.com>
2023-05-23 10:49:29 +0500

    fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,54 @@
 import React, { memo } from "react";
 import "./Header.css";
+import { AiOutlineSearch, AiOutlineShopping } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineHeart } from "react-icons/ai";
+import { MdMenu } from "react-icons/md"; // MdMenuOpen
+import { NavLink, Link } from "react-router-dom";
 
 export const Header = memo(() => {
   return (
     <header className="header">
       <div className="header-top">
         <div className="header-top__left">
-          <a href=".">Одежда</a>
+          <button type="button">
+            <MdMenu />
+          </button>
+
+          <Link to="/">Одежда</Link>
         </div>
 
         <div className="header-top__center">
-          <a href=".">Sale</a>
-          <a href=".">Женщинам</a>
-          <a href=".">Мужчинам</a>
-          <a href=".">детям</a>
+          <NavLink to="/sale">Sale</NavLink>
+          <NavLink to="/women">Женщинам</NavLink>
+          <NavLink to="/men">Мужчинам</NavLink>
+          <NavLink to="/children">детям</NavLink>
+        </div>
+
+        <div className="header-top-right">
+          <button>
+            <AiOutlineSearch />
+          </button>
+
+          <Link to="/login">
+            <AiOutlineUser />
+          </Link>
+
+          <Link to="/favorites">
+            <AiOutlineHeart />
+          </Link>
+
+          <Link to="/cart">
+            <AiOutlineShopping />
+          </Link>
         </div>
       </div>
+      <nav className="header-navbar">
+        <a href=".">новинки</a>
+        <a href=".">одежда</a>
+        <a href=".">Обувь</a>
+        <a href=".">аксессуары</a>
+        <a href=".">Любимые бренды</a>
+      </nav>
     </header>
   );
 });
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./Assets/Global.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Fragment>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Fragment>
 );
